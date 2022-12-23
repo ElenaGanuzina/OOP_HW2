d049136846a1176eb966b6581a6dae606e3da728
ElenaG <freshelf@yandex.ru>
2022-12-23 16:12:38 +0500

    add TeacherView

---
+++ view/TeacherView.java
@@ -0,0 +1,15 @@
+package Seminar2.view;
+
+import java.util.List;
+
+import Seminar2.data.User;
+
+public class TeacherView extends UserView {
+
+    @Override
+    protected User findTheBest(List<User> userList){
+        return null;
+    }
+        
+}
+
